5bd74d182a38205b5094054b967cec9be8930643
Toan <truongtoanlvc@gmail.com>
2020-12-18 03:42:07 +0700

    Login,logout, list user and permision per user with AuthGuard and Angular Materials

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,48 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './auth.guard';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { RegisterComponent } from './register/register.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'/',
+    pathMatch:'full'
+  },
+  // {
+  //   path:'users',
+  //   component:UsersComponent,
+  //   canActivate:[AuthGuard]
+  // },
+  // {
+  //   path:'users/permission',
+  //   component:UsersPermissionComponent,
+  //   canActivate:[AuthGuard]
+  // },
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {
+    path:'logout',
+    component:LogoutComponent
+  },
+  {
+    path:'register',
+    component:RegisterComponent
+  },
+  {
+    path:'user',
+    loadChildren:() => import('./user/user.module').then(m => m.UserModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
+  // imports: [RouterModule.forRoot(routes,{ enableTracing: true })],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,65 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import{HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
+import { FormsModule } from '@angular/forms';
+import { AuthService } from './auth.service';
+import { EventService } from './services/event.service';
+import { LogoutComponent } from './logout/logout.component';
+import { AuthGuard } from './auth.guard';
+import {TokenInterceptorService} from './token-interceptor.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatTableModule } from '@angular/material/table'  ;
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FilterPipe } from './user/pipe/filter-custom.pipe';
+import { UserModule } from './user/user.module';
+import { UserComponent } from './user/user.component';
+import { SharedModule } from './shared/shared.module';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    LogoutComponent,
+    LogoutComponent,
+    UserComponent,
+    // FilterPipe,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+    // UserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    // SharedModule,
+    // MatSliderModule,
+    // MatTableModule,
+    // MatPaginatorModule,
+    // MatTabsModule,
+    // MatListModule,
+    // MatIconModule,
+    // MatFormFieldModule,
+    // MatInputModule,
   ],
-  providers: [],
+  providers: [AuthService,EventService,AuthGuard,{
+    provide: HTTP_INTERCEPTORS,
+    useClass: TokenInterceptorService,
+    multi:true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
